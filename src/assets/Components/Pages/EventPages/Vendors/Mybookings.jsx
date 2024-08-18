b509563b7f75e41938de2382280c80717901fd1b
Suresh5026 <suresh361994@gmail.com>
2024-08-18 19:55:35 +0530

    commit12

--- src/assets/Components/Pages/EventPages/Vendors/Mybookings.jsx
+++ src/assets/Components/Pages/EventPages/Vendors/Mybookings.jsx
@@ -1,33 +1,32 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { Button, Container, Table } from "react-bootstrap";
 import axios from "axios";
-export default function Mybookings(){
-    const columHeadings = [
-        "S.No",
-        "User Name",
-        "Email ID",
-        "Contact Number",
-        "Event Name",
-        "Event Price",
-        "Event From Date",
-        "Event To Date",
-        "Guests Count",
-        "Status",
-        "Cancel Request"
-    ]    
-    const [booking, setBookings] = useState([]);
-    
-    useEffect(()=>{
-        const token = localStorage.getItem("token");
+export default function Mybookings() {
+  const columHeadings = [
+    "S.No",
+    "User Name",
+    "Email ID",
+    "Contact Number",
+    "Event Name",
+    "Event Price",
+    "Event From Date",
+    "Event To Date",
+    "Guests Count",
+    "Status",
+    "Action",
+  ];
+  const [booking, setBookings] = useState([]);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
     const fetchBookings = async () => {
       try {
         const response = await axios.get(
           "https://backendcapwedplanappevent.onrender.com/bookings/get-bookings",
           {
             headers: {
-              'Authorization': `Bearer ${token}`
-          },
-          
+              Authorization: `Bearer ${token}`,
+            },
           }
         );
         const bookingData = response.data.data;
@@ -40,43 +39,85 @@ export default function Mybookings(){
     if (token) {
       fetchBookings();
     }
+  }, []);
 
-    },[])
-
-    const handleCancel = async (id) => {
-      const token = localStorage.getItem("token");
-      try {
-
-      const confirmCancel = window.confirm("Are you sure you want to cancel this booking?");
+  const handleCancel = async (id) => {
+    const token = localStorage.getItem("token");
+    try {
+      const confirmCancel = window.confirm(
+        "Are you sure you want to cancel this booking?"
+      );
       if (!confirmCancel) {
-        return; // Exit if user does not confirm
+        return;
       }
-        await axios.put(
-          `https://backendcapwedplanappevent.onrender.com/bookings/cancel-booking/${id}`,
-          {},
-          {
-            withCredentials: true,
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        
-        setBookings((prevBookings) =>
-          prevBookings.map((booking) =>
-            booking._id === id ? { ...booking, status: "cancelled" } : booking
-          )
-        );
-      } catch (error) {
-        console.log("Error cancelling booking:", error);
-      }
-    };
+      await axios.put(
+        `https://backendcapwedplanappevent.onrender.com/bookings/cancel-booking/${id}`,
+        {},
+        {
+          withCredentials: true,
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
 
-    return(
-        <Container>
+      setBookings((prevBookings) =>
+        prevBookings.map((booking) =>
+          booking._id === id ? { ...booking, status: "cancelled" } : booking
+        )
+      );
+    } catch (error) {
+      console.log("Error cancelling booking:", error);
+    }
+  };
+  const handleOpenRazorpay = (data, userId, bookingId) => {
+    const options = {
+      key: "rzp_test_tGoWeh9ybvAQtC",
+      amount: Number(data.amount),
+      currency: "INR",
+      name: "Doctor Appointment App",
+      order_id: data.id,
+      notes: {
+        userId: userId,
+        bookingId: bookingId,
+      },
+      handler: function (response) {
+        axios
+          .post("https://backendcapwedplanappevent.onrender.com/payment/verify", {
+            response: response,
+            bookingId: bookingId,
+          })
+          .then((res) => {
+            console.log(res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      },
+    };
+    const rzp = new window.Razorpay(options);
+    rzp.open();
+  };
+
+  const handlePayment = (amount, userId, bookingId) => {
+    const data = {
+      amount: amount,
+      userId: userId,
+      bookingId: bookingId,
+    };
+    axios
+      .post("https://backendcapwedplanappevent.onrender.com/payment/orders", data)
+      .then((res) => {
+        console.log(res.data.data);
+        handleOpenRazorpay(res.data.data, userId, bookingId);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <Container>
       <div className="d-flex justify-content-between p-1">
         <h1>My Bookings</h1>
-        
       </div>
 
       <Table responsive>
@@ -100,19 +141,37 @@ export default function Mybookings(){
               <td>{item.toDate}</td>
               <td>{item.guests}</td>
               <td>{item.status}</td>
+
               <td>
-              {item.status !== "cancelled" && (
-                  <Button variant="danger" onClick={() => handleCancel(item._id)}>
-                    Cancel Booking
+                {item.status !== "cancelled" &&
+                  item.paymentStatus === "Pending" && (
+                    <Button
+                      variant="danger"
+                      onClick={() => handleCancel(item._id)}
+                    >
+                      Cancel Booking
+                    </Button>
+                  )}
+                {item.paymentStatus === "Paid" ? (
+                  "Paid"
+                ) : item.status !== "cancelled" &&
+                  item.paymentStatus !== "Paid" ? (
+                  <Button
+                    variant="success"
+                    onClick={() =>
+                      handlePayment(item.eventPrice, item.userId, item._id)
+                    }
+                  >
+                    Pay Now
                   </Button>
+                ) : (
+                  ""
                 )}
               </td>
-              
             </tr>
           ))}
         </tbody>
       </Table>
     </Container>
-
-    )
+  );
 }
--- src/assets/Components/Pages/Protected/Slides.jsx
+++ src/assets/Components/Pages/Protected/Slides.jsx
@@ -1,65 +1,48 @@
-import Carousel from 'react-bootstrap/Carousel';
+import Carousel from "react-bootstrap/Carousel";
 
-export default function Slides(){
-    return (
-        <Carousel data-bs-theme="dark">
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="/images/venues/m2.jpg"
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="/images/venues/m1.jpg"
-              alt="Second slide"
-            />
-            <Carousel.Caption>
-              
-              
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="/images/venues/m3.jpg"
-              alt="Third slide"
-            />
-            <Carousel.Caption>
-              
-       
-              
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="/images/venues/m4.jpg"
-              alt="fourth slide"
-            />
-            <Carousel.Caption>
-              
-       
-              
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="/images/venues/m5.webp"
-              alt="fifth slide"
-            />
-            <Carousel.Caption>
-              
-       
-              
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      );
+export default function Slides() {
+  return (
+    <Carousel data-bs-theme="dark">
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/images/venues/m2.jpg"
+          alt="First slide"
+        />
+        <Carousel.Caption></Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/images/venues/m1.jpg"
+          alt="Second slide"
+        />
+        <Carousel.Caption></Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/images/venues/m3.jpg"
+          alt="Third slide"
+        />
+        <Carousel.Caption></Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/images/venues/m4.jpg"
+          alt="fourth slide"
+        />
+        <Carousel.Caption></Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/images/venues/m5.webp"
+          alt="fifth slide"
+        />
+        <Carousel.Caption></Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 }
